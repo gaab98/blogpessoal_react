50883bc5b6cb3655f795c0a52f8003e7e53759cd
Gabriel José <gabrielt1406@gmail.com>
2026-09-22 20:11:00 -0300

    ✨ New Feature: Implementação Recurso Postagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,46 +1,37 @@
-
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
 
 function Home() {
+	return (
+		<>
+			<div className="bg-indigo-900 flex justify-center">
+				{/* Seção com 2 colunas */}
+				<div className="container grid grid-cols-1 md:grid-cols-2 text-white">
+					{/* Conteúdo de texto */}
+					<div className="flex flex-col gap-4 items-center justify-center py-4">
+						<h2 className="text-2xl md:text-5xl font-bold">Seja Bem Vindo(a)!</h2>
 
-  return (
+						<p className="text-xl">Expresse aqui seus pensamentos</p>
 
-    /* Container Princiapl */
-    <div className="bg-indigo-900 flex justify-center">
+						<div className="flex justify-around gap-4">
+							<ModalPostagem />
+						</div>
+					</div>
 
-      {/* Seção com 2 colunas */}
-      <div className="container grid grid-cols-1 md:grid-cols-2 text-white">
+					{/* Imagem da página home */}
+					<div className="flex justify-center">
+						<img
+							src="https://i.imgur.com/fyfri1v.png"
+							alt="Imagem da página Home"
+							className="w-2/3"
+						/>
+					</div>
+				</div>
+			</div>
 
-        {/* Conteúdo de texto */}
-        <div className="flex flex-col gap-4 items-center justify-center py-4">
-          <h2 className="text-2xl md:text-5xl font-bold">
-            Seja Bem Vinde!
-          </h2>
-
-          <p className="text-xl">
-            Expresse aqui seus pensamentos
-          </p>
-
-          <div className="flex justify-around gap-4">
-            <div className="rounded-lg border-2 border-solid border-white py-2 px-4">
-              Nova Postagem
-            </div>
-            
-          </div>
-        </div>
-
-        {/* Imagem da página home */}
-        <div className="flex justify-center">
-              <img 
-                src="https://i.imgur.com/fyfri1v.png"
-                alt="Imagem da página Home"
-                className="w-2/3"
-              />
-        </div>
-
-      </div>
-
-    </div>
-  )
+			<ListaPostagens />
+		</>
+	)
 }
 
 export default Home
